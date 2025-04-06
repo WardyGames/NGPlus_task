3f2c5b0c9b77acc5495ba3ae77e1fb3b669d797e
Wardy <34002671+WardyGames@users.noreply.github.com>
2025-04-06 15:41:18 +0200

    Movement + Camera
    Making of player movement script and camera controller via Cinemachine

--- Assets/Scripts/Interactables/InteractableItem.cs
+++ Assets/Scripts/Interactables/InteractableItem.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
 
-public class InteractableItem : MonoBehaviour
+public class InteractableItem : MonoBehaviour, IInteractable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+    public void Interact()
     {
-        
+        throw new System.NotImplementedException();
     }
 }
